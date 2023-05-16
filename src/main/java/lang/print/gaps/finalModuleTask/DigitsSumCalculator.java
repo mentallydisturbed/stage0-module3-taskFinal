7d4f944052a4dab07ef4a43b4aabb756bffc1d27
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 20:36:06 +0600

    mod3taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = number / 10000;
+        number %= 10000;
+        sum += number / 1000;
+        number %= 1000;
+        sum += number / 100;
+        number %= 100;
+        sum += number / 10;
+        number %= 10;
+        sum += number;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double res = temperatureCelsius* 9 / 5. + 32;
     }
 }
