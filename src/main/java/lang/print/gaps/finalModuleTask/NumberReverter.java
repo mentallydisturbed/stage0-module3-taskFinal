008f55e0632f2d84e22307cabf434367c6bd83f0
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 20:40:14 +0600

    fix2mod3taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -4,8 +4,4 @@ public class NumberReverter {
     public void revert(int number) {
         System.out.println((((number % 100) % 10) * 100 + ((number % 100)/ 10) * 10 + (number / 100)));
     }
-
-//    public static void main(String[] args) {
-//        new NumberReverter().revert(984);
-//    }
 }
